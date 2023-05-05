e23fe6858f304b5dec83d852535339c452c535e1
Mrugesh Tank <mrugesh@gteches.net>
2023-05-05 18:37:46 +0530

    Login screen background image set

--- MyTravel2/View/LoginView.swift
+++ MyTravel2/View/LoginView.swift
@@ -10,13 +10,22 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
 		ZStack {
-			Text("Hello")
+			Image(uiImage: UIImage(named: "img_background")!)
+				.resizable()
+				.aspectRatio(contentMode: .fill)
+				.clipped()
+				.ignoresSafeArea()
 		}
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
+	static var previews: some View {
+		LoginView()
+			.previewDevice("iPhone 14 Pro")
+			.previewDisplayName("iPhone 14 Pro")
+		LoginView()
+			.previewDevice("iPhone 8 Plus")
+			.previewDisplayName("iPhone 8 Plus")
+	}
 }
