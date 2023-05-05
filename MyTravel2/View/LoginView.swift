dd89e34823aa5e0e9bece1342b6cbd1a82801d9f
Mrugesh Tank <mrugesh@gteches.net>
2023-05-05 18:04:12 +0530

    • Alamofire and SDWebimage dependencies added to Project via SPM • git ignore added

---
+++ MyTravel2/View/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  MyTravel2
+//
+//  Created by Mrugesh Tank on 05/05/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+		ZStack {
+			Text("Hello")
+		}
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
