990104908c2b204dc84573fc1b155251ea9077d3
Swati Antala <swati@gteches.net>
2023-05-08 12:11:26 +0530

    • Set font • Add R.swift SPM

---
+++ MyTravel2/Helper/AppColor.swift
@@ -0,0 +1,19 @@
+//
+//  AppColor.swift
+//  MyTravel
+//
+//  Created by Xuser on 08/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum AppColor {
+	static let theme = Color(R.color.theme)
+	static let white = Color(R.color.white)
+
+	struct Text {
+		static let textPrimary = Color(R.color.textPrimary)
+		static let textSecondary = Color(R.color.textSecondary)
+	}
+}
---
+++ MyTravel2/Helper/AppFont.swift
@@ -0,0 +1,10 @@
+//
+//  AppFont.swift
+//  MyTravel
+//
+//  Created by Xuser on 08/05/23.
+//
+
+import Foundation
+import SwiftUI
+
