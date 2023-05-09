f25fd3d509192771148f58296d5bd92227bf534e
Swati Antala <swati@gteches.net>
2023-05-09 10:39:32 +0530

    • swiftLint script add • remove warnings • info.plist file add in resources folder

--- MyTravel2/Helper/AppFont.swift
+++ MyTravel2/Helper/AppFont.swift
@@ -8,7 +8,6 @@
 import Foundation
 import SwiftUI
 
-
 enum AppFont {
 	static func getFont(forStyle style: UIFont.TextStyle, forWeight weight: Font.Weight = .regular) -> Font? {
 		return Font.custom(R.font.montserratThin, size: UIFont.preferredFont(forTextStyle: style).pointSize).weight(weight)
