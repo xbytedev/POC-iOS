f67c2caa769b074ebcf1902de606d9eed4c9f3e7
Mrugesh Tank <mrugesh@gteches.net>
2023-05-15 12:59:11 +0530

    • Internal structure updated for OTP view

--- MyTravel2/View/OTPView.swift
+++ MyTravel2/View/OTPView.swift
@@ -9,82 +9,57 @@ import SwiftUI
 
 class ViewModel: ObservableObject {
 
+	let otpDigitCount = 4
+
 	@Published var otpField = "" {
 		didSet {
-			isNextTypedArr = Array(repeating: false, count: 4)
-			guard otpField.count <= 4,
+			isNextTypedArr = Array(repeating: false, count: otpDigitCount)
+			guard otpField.count <= otpDigitCount,
 				  otpField.last?.isNumber ?? true else {
 				otpField = oldValue
 				return
 			}
-			if otpField.count < 4 {
+			if otpField.count < otpDigitCount {
 				isNextTypedArr[otpField.count] = true
 			}
 		}
-	}
-	var otp1: String {
-		guard otpField.count >= 1 else {
-			return ""
-		}
-		return String(Array(otpField)[0])
-	}
-	var otp2: String {
-		guard otpField.count >= 2 else {
-			return ""
-		}
-		return String(Array(otpField)[1])
-	}
-	var otp3: String {
-		guard otpField.count >= 3 else {
-			return ""
-		}
-		return String(Array(otpField)[2])
-	}
-	var otp4: String {
-		guard otpField.count >= 4 else {
-			return ""
-		}
-		return String(Array(otpField)[3])
 	}
 
-	@Published var isNextTypedArr = Array(repeating: false, count: 4)
-	@Published var borderColor: Color = .black
+	@Published var isNextTypedArr = Array(repeating: false, count: 4) // otpDigitCount
 	@Published var isEditing = false {
 		didSet {
-			isNextTypedArr = Array(repeating: false, count: 4)
-			if isEditing && otpField.count < 4 {
+			isNextTypedArr = Array(repeating: false, count: otpDigitCount)
+			if isEditing && otpField.count < otpDigitCount {
 				isNextTypedArr[otpField.count] = true
 			}
 		}
+	}
+
+	func getSingleDigitFromOTP(atIndex index: Int) -> String {
+		guard otpField.count >= index + 1 else {
+			return ""
+		}
+		return String(otpField[otpField.index(otpField.startIndex, offsetBy: index)])
 	}
 }
 
 struct OTPView: View {
 	@StateObject var viewModel = ViewModel()
 
-	let textBoxWidth = UIScreen.main.bounds.width / 6
-	let textBoxHeight = UIScreen.main.bounds.width / 6
-	let spaceBetweenLines: CGFloat = 10
-	let paddingOfBox: CGFloat = 1
-	var textFieldOriginalWidth: CGFloat {
-		(textBoxWidth*6)+(spaceBetweenLines*3)+((paddingOfBox*2)*3)
-	}
-
 	var body: some View {
 		VStack {
 			ZStack {
-				HStack {
-					otpText(text: viewModel.otp1, isNextTyped: $viewModel.isNextTypedArr[0])
-					otpText(text: viewModel.otp2, isNextTyped: $viewModel.isNextTypedArr[1])
-					otpText(text: viewModel.otp3, isNextTyped: $viewModel.isNextTypedArr[2])
-					otpText(text: viewModel.otp4, isNextTyped: $viewModel.isNextTypedArr[3])
+				HStack(spacing: 24) {
+					ForEach(0..<viewModel.otpDigitCount, id: \.self) { index in
+						otpText(text: viewModel.getSingleDigitFromOTP(atIndex: index), isNextTyped: $viewModel.isNextTypedArr[index])
+					}
 				}
 				.frame(height: 64)
-				.padding(.horizontal, 64)
+				.padding(.horizontal, 32)
 				TextField("", text: $viewModel.otpField) { isEditing in
 					viewModel.isEditing = isEditing
 				}
-				.frame(width: viewModel.isEditing ? 0 : textFieldOriginalWidth, height: textBoxHeight)
+				.frame(width: viewModel.isEditing ? 0 : .infinity, height: 64)
 				.textContentType(.oneTimeCode)
 				.foregroundColor(.clear)
 				.accentColor(.clear)
@@ -110,6 +85,9 @@ struct OTPView: View {
 
 struct OTPView_Previews: PreviewProvider {
     static var previews: some View {
-        OTPView()
+		OTPView()
+			.previewDevice("iPhone 14 Pro")
+		OTPView()
+			.previewDevice("iPhone 8 Plus")
     }
 }
