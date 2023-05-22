711ad1edcf3805da0082418801dfdf67bff10fe7
Mrugesh Tank <mrugesh@gteches.net>
2023-05-22 17:10:28 +0530

    • Create group success screen strings fetched from the localizable table

--- MyTravel2/View/CreateGroupSuccessView.swift
+++ MyTravel2/View/CreateGroupSuccessView.swift
@@ -8,24 +8,27 @@
 import SwiftUI
 
 struct CreateGroupSuccessView: View {
+	let group: MTGroup
+	@Binding var shouldPresent: Bool
     var body: some View {
 		ZStack {
 			AppColor.theme.ignoresSafeArea()
 			VStack(spacing: 72) {
 				VStack(spacing: 36) {
 					Image(R.image.ic_right)
-					Text("Group\n" + "Hammiltons & Friedmmans" + "\ncreated!")
+					Text(R.string.localizable.groupNameCreated(group.name ?? ""))
 						.multilineTextAlignment(.center)
 						.foregroundColor(AppColor.Text.tertiary)
 						.font(AppFont.getFont(forStyle: .largeTitle, forWeight: .medium))
 				}
-				VStack() {
-					MTButton(isLoading: .constant(false), title: "Add Travelers", loadingTitle: "") {
+				VStack {
+					MTButton(isLoading: .constant(false), title: R.string.localizable.addTravelers(), loadingTitle: "") {
 					}
 					.inverted()
 					Button {
+						shouldPresent = false
 					} label: {
-						Text("Cancel")
+						Text(R.string.localizable.cancel)
 							.foregroundColor(AppColor.Text.tertiary)
 							.font(AppFont.getFont(forStyle: .headline, forWeight: .semibold))
 					}
@@ -38,6 +41,7 @@ struct CreateGroupSuccessView: View {
 
 struct CreateGroupSuccessView_Previews: PreviewProvider {
     static var previews: some View {
-		CreateGroupSuccessView()
+		let group = MTGroup(id: 0, name: "Hammiltons & Friedmmans", partnerID: 0, status: 0, createdAt: nil, updatedAt: nil)
+		CreateGroupSuccessView(group: group, shouldPresent: .constant(false))
     }
 }
