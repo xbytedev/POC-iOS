463a122b1323b7c28e160acf45be663439ed7a6f
Mrugesh Tank <mrugesh@gteches.net>
2023-06-15 13:02:18 +0530

    • Added group code to group listing parsing

--- MyTravel2/Model/MTGroup.swift
+++ MyTravel2/Model/MTGroup.swift
@@ -9,12 +9,14 @@ import Foundation
 
 struct MTGroup: Decodable {
 	let id: Int
+	let groupdCode: Int
 	let name: String?
 	let partnerID, status: Int?
 	let createdAt, updatedAt: String?
 
 	enum CodingKeys: String, CodingKey {
 		case id, name
+		case groupdCode = "group_code"
 		case partnerID = "partner_id"
 		case status
 		case createdAt = "created_at"
