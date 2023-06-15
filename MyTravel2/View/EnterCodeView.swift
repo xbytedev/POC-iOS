98c4bd12d97ad7342831147a5245f26bcf8bee6d
Mrugesh Tank <mrugesh@gteches.net>
2023-06-15 12:25:29 +0530

    • Enter code manually popup created and added to scan qr code

---
+++ MyTravel2/View/EnterCodeView.swift
@@ -0,0 +1,100 @@
+//
+//  EnterCodeView.swift
+//  MyTravel
+//
+//  Created by Mrugesh Tank on 15/06/23.
+//
+
+import SwiftUI
+
+struct EnterCodeView: View {
+	@State private var code: String = ""
+	@Binding var isPresenting: Bool
+	@State var configuration = UIConfiguration()
+	var addedToGroupSuccessfull: (_ code: String) -> Void
+
+    var body: some View {
+		ZStack {
+			Color.black
+				.ignoresSafeArea()
+				.opacity(isPresenting ? 0.4 : 0)
+				.animation(.easeInOut, value: isPresenting)
+			popupView
+				.scaleEffect(isPresenting ? 1 : 0)
+				.animation(.spring(), value: isPresenting)
+		}
+		.showAlert(isPresented: $configuration.alertPresent) {
+			Text(configuration.errorMeessage)
+		}
+    }
+
+	var headerView: some View {
+		HStack {
+			Text(R.string.localizable.enterCodeManually())
+				.font(AppFont.getFont(forStyle: .title1, forWeight: .bold))
+				.foregroundColor(AppColor.theme)
+			Spacer()
+			Button {
+				dismiss()
+			} label: {
+				Image(systemName: "xmark")
+					.font(.title)
+					.aspectRatio(1, contentMode: .fit)
+					.padding(8)
+					.background(Color.gray)
+					.clipShape(Circle())
+			}
+		}
+	}
+
+	var popupView: some View {
+		VStack {
+			headerView
+			MTTextField(label: R.string.localizable.enterCode(), valueStr: $code)
+				.padding(.bottom, 24)
+		}
+		.modifier(FormModifier())
+		.myOverlay(alignment: .bottom) {
+			MTButton(
+				isLoading: .constant(false), title: R.string.localizable.done(),
+				loadingTitle: R.string.localizable.addingToGroup()) {
+					handleAddToGroupAction()
+				}
+			.padding(.horizontal, 64)
+			.offset(x: 0, y: 20)
+		}
+	}
+
+	func handleAddToGroupAction() {
+		if code.isEmpty {
+			configuration.alertPresent = true
+			configuration.errorMeessage = R.string.localizable.pleaseEnterYourGroupName()
+		} else {
+			/*Task {
+				do {
+					configuration.isLoading = true
+					configuration.alertPresent = false
+					let group = try await viewModel.doCreateGroup(groupName: groupName)
+					self.configuration.isLoading = false
+					isPresenting = false
+					addedToGroupSuccessfull(group)
+					code = ""
+				} catch {
+					self.configuration.errorMeessage = error.localizedDescription
+					self.configuration.alertPresent = true
+					self.configuration.isLoading = false
+				}
+			}*/
+		}
+	}
+
+	private func dismiss() {
+		isPresenting = false
+	}
+}
+
+struct EnterCodeView_Previews: PreviewProvider {
+    static var previews: some View {
+		EnterCodeView(isPresenting: .constant(true), addedToGroupSuccessfull: { _ in })
+    }
+}
--- MyTravel2/View/ScanQRCodeView.swift
+++ MyTravel2/View/ScanQRCodeView.swift
@@ -8,45 +8,39 @@
 import SwiftUI
 
 struct ScanQRCodeView: View {
+	@State private var isPresenting: Bool = false
 	@ObservedObject var viewModel: ScanQRCodeViewModel
 
     var body: some View {
-		GeometryReader { geometryProxy in
-			let width = geometryProxy.size.width * 0.6
-			VStack(spacing: 48) {
-				VStack(spacing: 24) {
-					QRCodeScannerView(delegate: viewModel.qrCodeCameraDelegate)
-						.found(viewModel.onFound(qrCode:))
-						.tourchLight(isOn: viewModel.isTorchOn)
-						.interval(delay: viewModel.scanInterval)
-						.frame(width: width, height: width)
-					Text(viewModel.lastQRCode)
-					Text("Point the Camera at the QR Code")
-						.font(AppFont.getFont(forStyle: .title3))
-						.foregroundColor(AppColor.theme)
-						.frame(maxWidth: .infinity)
+		ZStack {
+			GeometryReader { geometryProxy in
+				let width = geometryProxy.size.width * 0.6
+				VStack(spacing: 48) {
+					VStack(spacing: 24) {
+						QRCodeScannerView(delegate: viewModel.qrCodeCameraDelegate)
+							.found(viewModel.onFound(qrCode:))
+							.tourchLight(isOn: viewModel.isTorchOn)
+							.interval(delay: viewModel.scanInterval)
+							.frame(width: width, height: width)
+						Text(viewModel.lastQRCode)
+						Text(R.string.localizable.pointTheCameraAtTheQrCode())
+							.font(AppFont.getFont(forStyle: .title3))
+							.foregroundColor(AppColor.theme)
+							.frame(maxWidth: .infinity)
+					}
+					MTButton(isLoading: .constant(false), title: R.string.localizable.enterCodeManually(), loadingTitle: "") {
+						isPresenting = true
+					}
 				}
-				MTButton(isLoading: .constant(false), title: "Enter Code Manually", loadingTitle: "") {
-				}
+				.padding(.top, geometryProxy.frame(in: .local).midY - (width / 2.0))
 			}
-			.padding(.top, geometryProxy.frame(in: .local).midY - (width / 2.0))
-		}
-    }
+			EnterCodeView(isPresenting: $isPresenting) { code
+				in
 
-	var someView: some View {
-		GeometryReader { geometryProxy in
-			VStack {
-				Rectangle()
-					.frame(width: geometryProxy.size.width * 0.6, height: geometryProxy.size.width * 0.6)
-					.position(x: geometryProxy.frame(in: .local).midX, y: geometryProxy.frame(in: .local).midY)
-				Text("Point the Camera at the QR Code")
-					.font(AppFont.getFont(forStyle: .body))
-				MTButton(isLoading: .constant(false), title: "Enter Code Manually", loadingTitle: "") {
-				}
-				Spacer()
 			}
 		}
-	}
+		.setThemeBackButton()
+    }
 }
 
 struct ScanQRCodeView_Previews: PreviewProvider {
